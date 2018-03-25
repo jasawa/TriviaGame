fb918998d1ef8fd45501917f6ba7ed485ba34b23
Jane Asawa <asawajane@gmail.com>
2018-03-25 14:47:02 -0700

    start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,13 @@
 
+function startQuestions() {
+    console.log("I'm in the questionnaire");
+    $("#start-button").empty();
+}
+
+
+$("#start-button").on("click", function() {
+    startQuestions();
+});
+
+
 console.log("The app.js is connected");
