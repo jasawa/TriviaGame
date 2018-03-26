88304c61954028ab0d720b76d5b55c86d539c8c3
Jane Asawa <asawajane@gmail.com>
2018-03-26 13:18:25 -0700

    added start and done buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,39 @@
 
+// variables
+var numCorrect = 0;
+var numWrong = 0;
+var numUnanswered = 0;
+
+
 function startQuestions() {
     console.log("I'm in the questionnaire");
+    // make start button disappear
     $("#start-button").empty();
+
+    // question form goes here
+
+
+    // make a done button
+    var doneButton = $("<button>");
+    console.log(doneButton);
+    doneButton.addClass("btn btn-primary btn-lg");
+    doneButton.text("Done");
+    $("#done-button").append(doneButton);
+    console.log("here");
+    // if done button clicked go to results page
+    $("#done-button").on("click", function() {
+        results();
+    });
 }
+
+function results() {
+    console.log("in results page now");
+    $("#done-button").empty();
+}
+
+
+
+
 
 
 $("#start-button").on("click", function() {
